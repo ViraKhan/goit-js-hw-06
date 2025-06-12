cbb316e0a13efd84ed41198e3cf8d46a552725e3
Vira Khan <vira.khan1@gmail.com>
2025-06-12 21:19:55 +0200

    update task2

--- js/task-2.js
+++ js/task-2.js
@@ -7,7 +7,24 @@ addItem(newItem) — приймає новий товар newItem і додає 
 removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості
  items об'єкта.
 */
+class Storage {
+    #items = [];
+    constructor(items) {
+        this.#items = items;
+    }
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItem) {
+        this.#items.push(newItem);
+        return this.#items; 
+    }
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter(item => item !== itemToRemove); 
+    }
 
+
+}
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
