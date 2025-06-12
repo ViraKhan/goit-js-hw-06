ab4b616a56a1d68e3709bea5610e112cbbb4c807
Vira Khan <vira.khan1@gmail.com>
2025-06-12 21:53:17 +0200

    Update task 3

--- js/task-3.js
+++ js/task-3.js
@@ -10,9 +10,26 @@ padStart(str) — отримує параметр str (рядок) і додає
 padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості value об'єкта,
  який викликає цей метод.
 */
-
-
+class StringBuilder {
+    #value;
+    constructor(initialValue) {
+        this.#value = initialValue;
+    }
+    getValue() {
+    return this.#value;
+    }
+    padEnd(str) {
+    this.#value += str;
+    }
+    padStart(str) {
+    this.#value = str + this.#value;
+    }
+    padBoth(str) {
+    this.#value = str + this.#value + str;
+    }
+}
 const builder = new StringBuilder(".");
+
 console.log(builder.getValue()); // "."
 builder.padStart("^");
 console.log(builder.getValue()); // "^."
